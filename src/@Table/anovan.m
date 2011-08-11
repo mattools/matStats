31b86c83441ddffa50d2cf0c93091171ff196291
dlegland <david.legland@nantes.inra.fr>
2011-08-11 13:49:21 +0000

    add multiple way anova (could be enhanced)

---
+++ src/@Table/anovan.m
@@ -0,0 +1,66 @@
+function [p, table, stats, terms] = anovan(data, groups, varargin)
+%ANOVAN Multi-way analysis of variance
+%
+%   anovan(DATA, groupsS)
+%   At least DATA or groups should be a data table with one column, the
+%   other argument, must be a data table or a column vector the same size.
+%   The functions formats inputs, and call the "anovan" function in the
+%   statistics toolbox.
+%
+%   anovan(..., NAME, VALUE)
+%   Specifies additional parameters that will be passed to the boxplot
+%   function.
+%
+%   Example
+%   anovan
+%
+%   See also
+%
+%
+% ------
+% Author: David Legland
+% e-mail: david.legland@grignon.inra.fr
+% Created: 2011-08-11,    using Matlab 7.9.0.529 (R2009b)
+% Copyright 2011 INRA - Cepia Software Platform.
+
+
+% extract input data
+if isa(data, 'Table')
+    dataValues = data.data;
+    
+else
+    % if data are numeric, assumes groups is Table object
+    dataValues = data;
+end
+
+% extract group values
+groupNames = {}; 
+if isa(groups, 'Table')
+    groupNames = groups.colNames;
+    
+    % create a new groupValues populated with level names
+    nGroups = columnNumber(groups);
+    groupValues = cell(1, nGroups);
+    for i = 1:nGroups
+        vals = groups.data(:, i);
+        if isFactor(groups, i)
+            % use cell array of strings for this group
+            levelNames = groups.levels{i};
+            groupValues{i} = levelNames(vals);
+            
+        else
+            % use numeric array for this group
+            groupValues{i} = vals;
+        end
+    end
+    
+else
+    % if groups is an array, keep it the same
+    groupValues = groups;
+    
+end
+
+[p, table, stats, terms] = anovan(dataValues, groupValues, ...
+    'varnames', groupNames, ...
+    varargin{:});
+
