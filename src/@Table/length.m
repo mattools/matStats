405996b1cc34a3cc2300410bb549049e4989bbe4
dlegland <david.legland@nantes.inra.fr>
2011-08-23 14:26:35 +0000

    add function length

---
+++ src/@Table/length.m
@@ -0,0 +1,17 @@
+function l = length(this)
+%LENGTH Number of rows in the table.
+%
+%   L = length(TAB)
+%   Returns the number of rows (observations) in the data table TAB.
+%   This is equivalent to the function size(TAB, 1);
+%
+%   See also
+%   size, ndims
+%
+% ------
+% Author: David Legland
+% e-mail: david.legland@grignon.inra.fr
+% Created: 2011-08-23,    using Matlab 7.9.0.529 (R2009b)
+% Copyright 2011 INRA - Cepia Software Platform.
+
+l = size(this.data, 1);
