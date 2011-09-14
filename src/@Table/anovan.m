dec34ea04746178c0393226c6bbb971a7f0e1287
dlegland <david.legland@nantes.inra.fr>
2011-09-14 07:04:32 +0000

    anovan: update doc

--- src/@Table/anovan.m
+++ src/@Table/anovan.m
@@ -1,21 +1,25 @@
 function [p, table, stats, terms] = anovan(data, groups, varargin)
 %ANOVAN Multi-way analysis of variance
 %
-%   anovan(DATA, groupsS)
+%   anovan(DATA, groups)
 %   At least DATA or groups should be a data table with one column, the
 %   other argument, must be a data table or a column vector the same size.
 %   The functions formats inputs, and call the "anovan" function in the
 %   statistics toolbox.
 %
 %   anovan(..., NAME, VALUE)
-%   Specifies additional parameters that will be passed to the boxplot
+%   Specifies additional parameters that will be passed to the anovan
+%   function.
+%
+%   [p table stats terms] = anovan(...)
+%   Returns additionnal results, that can be used with the 'multcompare'
 %   function.
 %
 %   Example
 %   anovan
 %
 %   See also
-%
+%     multcompare
 %
 % ------
 % Author: David Legland
@@ -60,6 +64,7 @@ else
     
 end
 
+% call the function from statistic toolbox with appropriate parameters
 [p, table, stats, terms] = anovan(dataValues, groupValues, ...
     'varnames', groupNames, ...
     varargin{:});
