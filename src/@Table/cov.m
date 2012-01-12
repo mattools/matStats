c42a6afe6a1a3b60acbbbccb37730e197b894b51
dlegland <david.legland@nantes.inra.fr>
2012-01-12 13:46:00 +0000

    add the "cov" function

--- src/@Table/cov.m
+++ src/@Table/cov.m
@@ -1,13 +1,13 @@
-function res = corrcoef(this)
-%CORRCOEF Correlation coefficients of table data
+function res = cov(this)
+%COV Covariance matrix of the data table
 %
-%   CORR = corrcoef(TAB)
+%   C = cov(TAB)
 %
 %   Example
-%   corrcoef
+%   cov(TAB)
 %
 %   See also
-%   std, cov
+%   std, var, corrcoef
 %
 % ------
 % Author: David Legland
@@ -15,6 +15,6 @@ function res = corrcoef(this)
 % Created: 2012-01-10,    using Matlab 7.9.0.529 (R2009b)
 % Copyright 2012 INRA - Cepia Software Platform.
 
-data = corrcoef(this.data);
+data = cov(this.data);
 
 res = Table(data, 'rowNames', this.colNames, 'colNames', this.colNames);
