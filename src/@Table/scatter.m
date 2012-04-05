769b593eb7601001628ae01c3bd43f1bdfa70797
dlegland <david.legland@nantes.inra.fr>
2012-04-05 09:25:27 +0000

    scatter: better support of factors

--- src/@Table/scatter.m
+++ src/@Table/scatter.m
@@ -1,21 +1,29 @@
 function varargout = scatter(this, varargin)
 %SCATTER Scatter plot of table data
 %
+%   scatter(TAB1, TAB2)
+%   Use two tables, with one column each, that respectively specifies the x
+%   and y coordinates.
+%
 %   scatter(TAB, VAR1, VAR2)
 %   Scatters the data in the table TAB by using corodinates VAR1 and VAR2.
 %   TABLE is a Table object, and VAR1 and VAR2 are either indices or names
 %   of 2 columns in the table.
-%
-%   scatter(TAB1, TAB2)
-%   Use two tables, with one column each, that respectively specifies the x
-%   and y coordinates.
 %
 %   scatter(..., STYLE)
 %   scatter(..., PARAM, VALUE)
 %   Specifies drawing options.
 %
 %   Example
-%     scatter
+%   % scatter petal length values against petla width
+%     iris = Table.read('fisherIris.txt');
+%     figure; set(gca, 'fontsize', 14);
+%     scatter(iris('PetalWidth'), iris('PetalLength'))
+%
+%   % scatter petal length values against species
+%     iris = Table.read('fisherIris.txt');
+%     figure; set(gca, 'fontsize', 14);
+%     scatter(iris('Species'), iris('PetalLength'), 's')
 %
 %   See also
 %     plot
@@ -36,6 +44,7 @@ if size(this.data, 2) == 1
     end
     
     xdata = this.data(:, 1);
+    indx = 1;
     nameX = this.colNames{1};
     
     var = varargin{1};
@@ -52,13 +61,13 @@ else
     
     % index of first column
     var1 = varargin{1};
-    indx = this.columnIndex(var1);
+    indx = columnIndex(this, var1);
     xdata = this.data(:, indx(1));
     nameX = this.colNames{indx(1)};
 
     % index of second column
     var2 = varargin{2};
-    indy = this.columnIndex(var2);
+    indy = columnIndex(this, var2);
     ydata = this.data(:, indy(1));
     nameY = this.colNames{indy(1)};
     
@@ -71,7 +80,22 @@ if isempty(varargin)
 end
 
 % scatter plot of selected columns
-h = scatter(xdata, ydata, varargin{:});
+ax = gca;
+h = scatter(ax, xdata, ydata, varargin{:});
+
+if isFactor(this, indx(1))
+    levels = this.levels{indx(1)};
+    if isnumeric(levels)
+        set(ax, 'xtick', levels);
+        
+    else
+        nLevels = length(levels);
+        set(ax, 'xlim', [.5 nLevels+.5]);
+        set(ax, 'xtick', 1:nLevels);
+        set(ax, 'xtickLabel', levels);
+    end
+end
+
 
 % add plot annotations
 xlabel(nameX, 'Interpreter', 'none');
