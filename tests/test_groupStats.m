b5e3f47800bf88e6ef909a43c062e5301a1da69f
dlegland <david.legland@nantes.inra.fr>
2012-04-20 17:45:33 +0000

    add test for groupstats

---
+++ tests/test_groupStats.m
@@ -0,0 +1,38 @@
+function test_suite = test_groupStats(varargin) %#ok<STOUT>
+%TEST_GROUPSTATS  Test case for the file groupStats
+%
+%   Test case for the file groupStats
+
+%   Example
+%   test_groupStats
+%
+%   See also
+%
+%
+% ------
+% Author: David Legland
+% e-mail: david.legland@grignon.inra.fr
+% Created: 2012-04-20,    using Matlab 7.9.0.529 (R2009b)
+% Copyright 2012 INRA - Cepia Software Platform.
+
+initTestSuite;
+
+function test_Iris %#ok<*DEFNU>
+% Test call of function without argument
+iris = Table.read('fisherIris.txt');
+res = groupStats(iris(:, 1:4), iris('Species'));
+
+exp = [3 4];
+assertElementsAlmostEqual(exp, size(res));
+
+function test_SeveralOutputs
+
+iris = Table.read('fisherIris.txt');
+stats = {@mean, @std, @numel};
+[res1 res2 res3] = groupStats(iris(:, 1:4), iris('Species'), stats);
+
+exp = [3 4];
+assertElementsAlmostEqual(exp, size(res1));
+assertElementsAlmostEqual(exp, size(res2));
+assertElementsAlmostEqual(exp, size(res3));
+
