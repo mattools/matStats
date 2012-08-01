8c1ed925a4a7122b06863c924b3544847b3294c1
dlegland <david.legland@nantes.inra.fr>
2012-08-01 07:35:18 +0000

    add bsxfun

---
+++ src/@Table/bsxfun.m
@@ -0,0 +1,36 @@
+function res = bsxfun(fun, this, that)
+%BSXFUN Binary Singleton Expansion Function for Table
+%
+%   RES = bsxfun(FUN, A, B)
+%
+%   Example
+%     % Compute binary expansion with minus on iris table
+%     tab = Table.read('fisherIris.txt');
+%     tab = tab(:, 1:end-1);
+%     means = tab;
+%     res = bsxfun(@minus, tab, means);
+%
+%   See also
+%
+%
+% ------
+% Author: David Legland
+% e-mail: david.legland@grignon.inra.fr
+% Created: 2012-08-01,    using Matlab 7.9.0.529 (R2009b)
+% Copyright 2012 INRA - Cepia Software Platform.
+
+[data1 data2 parent] = parseInputCouple(this, that);
+
+% error checking
+if hasFactors(parent)
+    error('Can not compute bsxfun for table with factors');
+end
+
+% call bsxfun on inner data
+newData = bsxfun(fun, data1, data2);
+
+% newColNames = strcat(names1, '+', names2);
+
+% create result array
+res = Table.create(newData, ...
+    'parent', parent);
---
+++ tests/test_bsxfun.m
@@ -0,0 +1,30 @@
+function test_suite = test_bsxfun(varargin) %#ok<STOUT>
+%TEST_BSXFUN  Test case for the file bsxfun
+%
+%   Test case for the file bsxfun
+
+%   Example
+%   test_bsxfun
+%
+%   See also
+%
+%
+% ------
+% Author: David Legland
+% e-mail: david.legland@grignon.inra.fr
+% Created: 2012-08-01,    using Matlab 7.9.0.529 (R2009b)
+% Copyright 2012 INRA - Cepia Software Platform.
+
+initTestSuite;
+
+function test_Simple %#ok<*DEFNU>
+
+tab = Table.read('fisherIris.txt');
+tab = tab(:, 1:end-1);
+
+means = tab;
+
+res = bsxfun(@minus, tab, means);
+
+assertEqual(size(tab), size(res));
+
