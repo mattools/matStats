6fa84f1f323ce9ecdbbc656b98bf6ae0da58a420
dlegland <david.legland@inra.fr>
2017-06-01 11:45:12 +0200

    Anova/coefficients changes the behavior of the coefficients method to return coefficients instead of displaying them

--- src/@Anova/coefficients.m
+++ src/@Anova/coefficients.m
@@ -1,16 +1,23 @@
-function lines = coefficients(this)
-%COEFFICIENTS Display mean values of anova coefficients in a figure
+function coeffs = coefficients(this, factorName)
+%COEFFICIENTS Return the coefficients computed for a factor
 %
 %   coefficients(ANOVA)
-%   Opens a new figure to display result of an anova.
-%
-%   COEFFS = coefficients(ANOVA)
-%   Return the results in a cell array of lines.
+%   plotCoefficients(ANOVA, FACTORNAME)
 %
 %   Example
-%     coefficients
+%     % Compute Anova on Iris dataset, and display species coefficients
+%     iris = Table.read('fisherIris');
+%     res = Anova(iris('SepalLength'), iris('Species'));
+%     coefficients(res, 'Species')
+%     ans =
+%                               1
+%         Setosa            5.006
+%         Versicolor        5.936
+%         Virginica         6.588
 %
+%     
 %   See also
+%     showCoefficients, 
 %
 
 % ------
@@ -19,116 +26,41 @@ function lines = coefficients(this)
 % Created: 2011-08-11,    using Matlab 7.9.0.529 (R2009b)
 % Copyright 2011 INRA - Cepia Software Platform.
 
-stats = this.stats;
-nCoefs = length(stats.coeffnames);
 
-nChars = max(cellfun(@length, stats.coeffnames));
-valFormat = createFormattingString(stats.coeffs);
-format = [sprintf('%%-%ds: ', nChars) valFormat];
+%% Process input arguments
+
+if iscell(factorName) && length(factorName) > 1
+    error('Can process only one factor');
+end
+
+if ~strcmp(this.stats.varnames, factorName)
+    error(['Could not identify the factor: ' factorName]);
+end
+
+% index of the factor in the list of ANOVA factors
+indFactor = find(strcmp(this.stats.varnames, factorName));
+
+% levels of the factor (for populating results table)
+levels = this.stats.grpnames{indFactor};
+nLevels = this.stats.nlevels(indFactor);
 
 
-coeffFactorMatrix = stats.vars;
+%% Compute coefficient for each level
 
-nCoefsByComb = stats.termcols;
+% get global effect of the anova
+intercept = this.stats.coeffs(1);
 
-nFactorCombs = length(nCoefsByComb); 
+% number of terms before the terms of the current factor
+coeffOffset = sum(this.stats.termcols(1:indFactor));
 
+coeffs = Table(zeros(nLevels, 1), {this.varName});
 
-nLines = nCoefs + 3 * nFactorCombs;
-lines = cell(nLines, 1);
-
-iLine = 1;
-iCoef = 1;
-
-for i = 1:nFactorCombs
-    lines{iLine} = '';
-    iLine = iLine + 1;
+for i = 1:nLevels
+    % inedx of coefficient in the coefficient matrix
+    indCoeff = coeffOffset + i;
     
-    if iCoef == 1
-        lines{iLine} = 'Intercept Value';
-    else
-        indFactors = find(coeffFactorMatrix(iCoef, :) > 0);
-        if length(indFactors) == 1
-            lines{iLine} = sprintf('Estimated values for factor %s', ...
-                stats.varnames{indFactors(1)});
-        else
-            lines{iLine} = 'Estimated interactions';
-        end
-    end
-    iLine = iLine + 1;
+    coeffs(i) = intercept + this.stats.coeffs(indCoeff);
     
-    for j = 1:nCoefsByComb(i)
-        coeffName = stats.coeffnames{iCoef};
-        
-        indFactors = coeffFactorMatrix(iCoef, :);
-        numFactors = sum(indFactors > 0);
-        
-        % initialize to the value of intercept
-        fittedValue = stats.coeffs(1);
-
-        % if current coefficient refers to an interaction, we need to
-        % add the contributions of individual factors
-        if numFactors > 1
-            for k = 1:length(indFactors)
-                level = coeffFactorMatrix(iCoef, k);
-                if level == 0
-                    continue;
-                end
-                
-                ind = find(coeffFactorMatrix(:,k) == level, 1, 'first');
-                fittedValue = fittedValue + stats.coeffs(ind);
-            end
-        end
-        
-        % also add the coefficient value
-        if iCoef > 1
-            fittedValue = fittedValue + stats.coeffs(iCoef);
-        end
-        
-        lines{iLine} = sprintf(format, ...
-            coeffName, fittedValue);
-        
-        iCoef = iCoef + 1;
-        iLine = iLine + 1;
-    end
+    coeffs.rowNames{i} = levels{i};
 end
 
-
-% display result in a table
-figure;
-uicontrol(gcf, ...
-    'Style', 'text', ...
-    'Units', 'normalized', ...
-    'Position', [0 0 1 1], ...
-    'BackgroundColor', [1 1 1], ...
-    'Max', 2, ...
-    'HorizontalAlignment', 'left', ...
-    'FontName', 'FixedWidth', ...
-    'String', lines);
-
-
-function format = createFormattingString(values)
-
-nDigitsMax = 6;
-
-% compute number of digits before and after the decimal separator
-nSigDigits = ceil(max(log10(abs(values))));
-nDecDigits = max(nDigitsMax - nSigDigits - 1, 0);
-
-% number of significant digits
-nDigits = nSigDigits + nDecDigits;
-
-% add one digit for the sign
-signDigit = '';
-if any(values < 0)
-    nDigits = nDigits + 1;
-    signDigit = ' ';
-end
-
-% add one digit for the decimal separator
-if any((values - floor(values)) > 0)
-    nDigits = nDigits + 1;
-end
-
-% create formatting string
-format = sprintf('%%%s%d.%df', signDigit, nDigits, nDecDigits);
--- src/@Anova/plotCoefficients.m
+++ src/@Anova/plotCoefficients.m
@@ -19,50 +19,18 @@ function plotCoefficients(this, factorName)
 % Created: 2017-05-30,    using Matlab 9.1.0.441655 (R2016b)
 % Copyright 2017 INRA - Cepia Software Platform.
 
-
-%% Process input arguments
-
-if iscell(factorName) && length(factorName) > 1
-    error('Can process only one factor');
-end
-
-if ~strcmp(this.stats.varnames, factorName)
-    error(['Could not identify the factor: ' factorName]);
-end
-
-indFactor = find(strcmp(this.stats.varnames, factorName));
-
-nLevels = this.stats.nlevels(indFactor);
-
-levels = this.stats.grpnames{indFactor};
-
-
-%% Compute coefficient for each level
-
-% get global effect of the anova
-intercept = this.stats.coeffs(1);
-
-% number of terms before the terms of the current factor
-coeffOffset = sum(this.stats.termcols(1:indFactor));
-
-coeffs = Table(zeros(nLevels, 1));
-
-for i = 1:nLevels
-    % inedx of coefficient in the coefficient matrix
-    indCoeff = coeffOffset + i;
-    
-    coeffs(i) = intercept + this.stats.coeffs(indCoeff);
-    
-    coeffs.rowNames{i} = levels{i};
-end
-
+coeffs = coefficients(this, factorName);
+nLevels = size(coeffs, 1);
 
 %% Display
 
+% plot as curve
 figure;
 plot(1:nLevels, coeffs.data);
 xlim([0 nLevels+1]);
+
+% annotate the graph
 xlabel(factorName);
 set(gca, 'xtick', 1:nLevels);
-set(gca, 'xticklabel', levels);
+set(gca, 'xticklabel', coeffs.rowNames);
 
