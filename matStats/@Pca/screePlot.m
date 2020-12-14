bc41bdd8303b99da793cf67849f370f27558ee38
dlegland <david.legland@inrae.fr>
2020-12-14 17:53:01 +0100

    Pca/screePlot.m: add possibility to specify number of components

--- matStats/@Pca/screePlot.m
+++ matStats/@Pca/screePlot.m
@@ -2,7 +2,11 @@ function varargout = screePlot(obj, varargin)
 % Display the scree plot of the PCA result.
 %
 %   screePlot(PCA)
-%   Display the scree plot of the PCA object in a new figure.
+%   Displays the scree plot of the PCA object in a new figure.
+%
+%   screePlot(PCA, NC)
+%   Chooses the number of components to display. NC must be lower than the
+%   number of components of the PCA result.
 %
 %   H = screePlot(PCA)
 %   Returns a handle to the created figure.
@@ -19,9 +23,19 @@ function varargout = screePlot(obj, varargin)
 
 % ------
 % Author: David Legland
-% e-mail: david.legland@inra.fr
+% e-mail: david.legland@inrae.fr
 % Created: 2013-02-27,    using Matlab 7.9.0.529 (R2009b)
 % Copyright 2013 INRA - Cepia Software Platform.
+
+% Number of components to display
+nc = 10;
+if ~isempty(varargin) 
+    var1 = varargin{1};
+    if isnumeric(var1) && isscalar(var1)
+        nc = var1;
+        varargin(1) = [];
+    end
+end
 
 
 % extract data
@@ -29,22 +43,22 @@ name    = obj.TableName;
 coord   = obj.Scores.Data;
 values  = obj.EigenValues.Data;
 
+% number of components to display
+nc = min(nc, size(coord, 2));
+
 % distribution of the first 10 eigen values
 h = figure('Name', 'PCA - Eigen Values', 'NumberTitle', 'off');
 if ~isempty(varargin)
     set(gca, varargin{:});
 end
 
-% number of components to display
-nx = min(10, size(coord, 2));
-
 % scree plot
-bar(1:nx, values(1:nx, 2));
+bar(1:nc, values(1:nc, 2));
 % hold on;
 % plot(1:nx, values(1:nx, 3), 'color', 'r', 'linewidth', 2);
 
 % setup graph
-xlim([0 nx+1]);
+xlim([0 nc+1]);
 
 % annotations
 xlabel('Number of components');
